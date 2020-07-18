e88d90cff00f6221ed9984a72f86b223ec4c7459
Khalakuzzaman Apon <khalakuzzamanapon5@gmail.com>
2020-07-18 08:48:55 +0600

    inintial setup up to oh my zsh

---
+++ ubuntu.sh
@@ -0,0 +1,6 @@
+sudo apt install zsh curl git sed
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+nano ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
