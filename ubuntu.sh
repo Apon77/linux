77e449b8debd440ea34841fe3425ad35b48de308
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2020-09-23 01:21:42 +0600

    p10 via sed

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,10 +2,10 @@ sudo apt install zsh curl git sed
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-#nano ~/.zshrc
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-nano ~/.zshrc
-ZSH_THEME="powerlevel10k/powerlevel10k"
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k/powerlevel10k"/g' ~/.zshrc
+#nano ~/.zshrc
+#ZSH_THEME="powerlevel10k/powerlevel10k"
 #p10k configure
 source ~/.zshrc
