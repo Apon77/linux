f152620e69f69716a869aa43118ed81ec5efcf8c
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2020-09-22 09:35:22 +0600

    Adding p10k configure

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,3 +7,5 @@ sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 nano ~/.zshrc
 ZSH_THEME="powerlevel10k/powerlevel10k"
+#p10k configure
+source ~/.zshrc
