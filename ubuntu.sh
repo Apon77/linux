21bebd1674ed12f20ca69c73e7102915f0b95b27
Khalakuzzaman Apon <khalakuzzamanapon5@gmail.com>
2020-09-23 18:57:17 +0600

    Editing commands url

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,3 +1,3 @@
 sudo apt install zsh curl git sed
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sh -c "$(curl -fsSL https://github.com/Apon77/linux/raw/junk/commands.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Apon77/linux/junk/commands.sh)"
