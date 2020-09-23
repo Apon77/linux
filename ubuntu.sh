643404fdeb666ba74e394bec327b45a087b04024
Khalakuzzaman Apon <khalakuzzamanapon5@gmail.com>
2020-09-23 18:50:07 +0600

    Moved all commands to another location for easy processing

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,12 +1,3 @@
 sudo apt install zsh curl git sed
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
-sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
-curl https://raw.githubusercontent.com/Apon77/linux/alias/easy >> ~/.zshrc
-#nano ~/.zshrc
-#ZSH_THEME="powerlevel10k/powerlevel10k"
-#p10k configure
-source ~/.zshrc
+sh -c "$(curl -fsSL https://github.com/Apon77/linux/raw/junk/commands.sh)"
