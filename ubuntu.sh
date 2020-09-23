39874a1ada514644f288a4bc6a9df027f780b0de
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2020-09-23 17:00:22 +0600

    Fix typo "

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugi
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
-sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
 #nano ~/.zshrc
 #ZSH_THEME="powerlevel10k/powerlevel10k"
 #p10k configure
