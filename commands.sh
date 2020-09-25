7328c5e29d1fff7071eceec1a9a81bc51251bad4
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2020-09-25 08:07:38 +0600

    Use bash to execute

--- commands.sh
+++ commands.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
