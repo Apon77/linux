d45c88381c4a85dc8bce4aa7e6ba8d56c07a9e58
Khalakuzzaman Apon <khalakuzzamanapon5@gmail.com>
2020-09-26 21:46:32 +0600

    Since source ~/.zshrc is not working automatically
    So, we need to do this mannually

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,3 +1,4 @@
 sudo apt install zsh curl git sed
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Apon77/linux/junk/commands.sh)"
+source ~/.zshrc
