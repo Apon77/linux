cd9d398f6af8d97286ccf05334b69bc0de3c2fac
Khalakuzzaman Apon <khalakuzzamanapon5@gmail.com>
2020-09-28 20:11:05 +0600

    Instruction how to use

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,3 +1,4 @@
+#Type these 4 lines one by one
 sudo apt install zsh curl git sed
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Apon77/linux/junk/commands.sh)"
