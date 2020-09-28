c826f2d67245a67272871e4a612d10b7427a083c
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2020-09-28 19:54:36 +0600

    Arch needs only pacman to be changed

---
+++ arch.sh
@@ -0,0 +1,4 @@
+sudo pacman -S zsh curl git sed
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Apon77/linux/junk/commands.sh)"
+source ~/.zshrc
