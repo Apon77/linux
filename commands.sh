72190f4d904dbe104bb31dec70b60bb1b9b5adab
Khalakuzzaman Apon <khalakuzzamanapon5@gmail.com>
2020-09-29 12:30:49 +0600

    Adding Fish shell like history search feature
    ping up/down arrow will show all ping contained command you typed before (from history)

--- commands.sh
+++ commands.sh
@@ -2,7 +2,9 @@
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
+
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions history-substring-search zsh-syntax-highlighting)/g' ~/.zshrc
 sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
 curl https://raw.githubusercontent.com/Apon77/linux/junk/easy >> ~/.zshrc
 #nano ~/.zshrc
