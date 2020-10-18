1a5ec7461c11af5493d40303478c0f80c4613b57
Apon77 <khalakuzzamanapon5@gmail.com>
2020-10-19 01:32:19 +0600

    Fix easy alias

--- commands.sh
+++ commands.sh
@@ -7,7 +7,7 @@ git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search ~/
 #zsh-syntax-highlighting must be the last plugin sourced.
 sed -i 's/plugins=(git)/plugins=(git z zsh-autosuggestions history-substring-search zsh-syntax-highlighting)/g' ~/.zshrc
 sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
-curl https://raw.githubusercontent.com/Apon77/linux/junk/easy -o ~/.oh-my-zsh/custom/easy.zsh
+curl https://raw.githubusercontent.com/Apon77/linux/junk/easy > ~/.oh-my-zsh/custom/easy.zsh
 #nano ~/.zshrc
 #ZSH_THEME="powerlevel10k/powerlevel10k"
 #p10k configure
