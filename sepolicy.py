8aab53e149feef035162867ab7a74c13dcb3d036
Apon77 <khalakuzzamanapon5@gmail.com>
2020-10-28 08:12:11 +0600

    Remove duplicate lines

--- sepolicy.py
+++ sepolicy.py
@@ -11,9 +11,14 @@ for lines in logs:
     if 'avc:' in lines:
         if 'denied' in lines:
             avc_list.append(lines)
+
+fix = []
 for item in avc_list:
     allow1 = item.split('scontext=u:r:')[1].split(':')[0]
     allow2 = item.split('scontext=u:r:')[1].split(':')[3]
     allow3 = item.split('tclass=')[1].split()[0]
     allow4 = item.split('denied')[1].split('for')[0].strip()
-    print (f'allow {allow1} {allow2}:{allow3} {allow4}')
+    fix.append(f'allow {allow1} {allow2}:{allow3} {allow4}')
+fix = list(dict.fromkeys(fix))
+for item in fix:
+    print (item)
