f8ddb595aa749f3e8f1bb7b2d6955beee09bddf7
Apon77 <khalakuzzamanapon5@gmail.com>
2020-11-01 18:46:13 +0600

    Adding extract plugin

--- commands.sh
+++ commands.sh
@@ -5,10 +5,10 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/cu
 git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
 
 #zsh-syntax-highlighting must be the last plugin sourced.
-sed -i 's/plugins=(git)/plugins=(git z zsh-autosuggestions history-substring-search zsh-syntax-highlighting)/g' ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(git z zsh-autosuggestions history-substring-search zsh-syntax-highlighting extract)/g' ~/.zshrc
 sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
 curl https://raw.githubusercontent.com/Apon77/linux/junk/easy > ~/.oh-my-zsh/custom/easy.zsh
 #nano ~/.zshrc
 #ZSH_THEME="powerlevel10k/powerlevel10k"
 #p10k configure
-#source ~/.zshrc
+source ~/.zshrc
