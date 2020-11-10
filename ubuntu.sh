84f1a84eadd9f8666fd18cff322970862a2234d7
Apon77 <khalakuzzamanapon5@gmail.com>
2020-11-11 04:10:26 +0600

    Auto yes to install packages

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,5 @@
 #Type these 4 lines one by one
-sudo apt install zsh curl git sed
+sudo apt install zsh curl git sed -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Apon77/linux/junk/commands.sh)"
 source ~/.zshrc
