8ff3c4181aa3e0ec5b9b48fd9a47d9aed8a98134
Apon77 <khalakuzzamanapon5@gmail.com>
2021-01-05 09:18:08 +0600

    gdj alias

--- personal_variables.zsh
+++ personal_variables.zsh
@@ -5,3 +5,4 @@ git config --global core.editor vim
 
 export TZ='Asia/Dhaka'
 alias gdup='gdrive upload -p 1DuHbMh4ogKDGbjSNP-7xLPY3S1KW1Z1i'
+alias gdj='gdrive upload -p 1-0kgxGSKeco0sZsuZ0Vsk4fM1-Vs8T07'
