1e761987584c3fbddcbdb5200ade7f1ddbc9715f
Apon77 <khalakuzzamanapon5@gmail.com>
2021-01-09 11:33:42 +0600

    fine

--- aliases.sh
+++ aliases.sh
@@ -2,7 +2,6 @@
 alias gcp='gpick'
 alias gpc='gpick'
 alias c='clear'
-alias src='source ~/.zshrc'
 alias n='nano'
 alias v='vim'
 alias tx='tmux'
@@ -23,10 +22,7 @@ alias rmrf='rm -rf'
 alias path='echo -e ${PATH//:/\\n}'
 alias myip="curl http://ipecho.net/plain; echo"
 alias st2='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -'
-alias easy="curl https://raw.githubusercontent.com/Apon77/linux/junk/easy.zsh > ~/.oh-my-zsh/custom/easy.zsh; source ~/.zshrc"
-alias common="curl https://raw.githubusercontent.com/Apon77/linux/junk/common.sh > ~/.oh-my-zsh/custom/common.zsh; source ~/.zshrc"
 alias rs='repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j'
-alias apon="curl https://raw.githubusercontent.com/Apon77/linux/junk/personal_variables.zsh > ~/.oh-my-zsh/custom/personal_variables.zsh; source ~/.zshrc"
 alias f='free -h'
 alias sshn='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 alias gssh='gcloud cloud-shell ssh'
