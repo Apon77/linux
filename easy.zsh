ddd3905bbe819dc88581f5678f32637f7ac6d5b6
Apon77 <khalakuzzamanapon5@gmail.com>
2021-01-09 15:59:16 +0600

    nb

--- easy.zsh
+++ easy.zsh
@@ -2,6 +2,8 @@ ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
 # aliases
 alias src='source ~/.zshrc'
+alias nz='nano ~/.zshrc'
+alias vz='vim ~/.zshrc'
 alias a="curl https://raw.githubusercontent.com/Apon77/linux/junk/aliases.sh > ~/.oh-my-zsh/custom/aliases.zsh; source ~/.zshrc"
 alias easy="curl https://raw.githubusercontent.com/Apon77/linux/junk/easy.zsh > ~/.oh-my-zsh/custom/easy.zsh; source ~/.zshrc"
 alias fn="curl https://raw.githubusercontent.com/Apon77/linux/junk/functions.sh > ~/.oh-my-zsh/custom/functions.zsh; source ~/.zshrc"
