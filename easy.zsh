dfabe4ea2914267363614cf8bfe5e19b70799fdb
Apon77 <khalakuzzamanapon5@gmail.com>
2021-01-09 12:08:31 +0600

    personal

--- easy.zsh
+++ easy.zsh
@@ -5,4 +5,4 @@ alias src='source ~/.zshrc'
 alias a="curl https://raw.githubusercontent.com/Apon77/linux/junk/aliases.sh > ~/.oh-my-zsh/custom/aliases.zsh; source ~/.zshrc"
 alias easy="curl https://raw.githubusercontent.com/Apon77/linux/junk/easy.zsh > ~/.oh-my-zsh/custom/easy.zsh; source ~/.zshrc"
 alias fn="curl https://raw.githubusercontent.com/Apon77/linux/junk/funcions.sh > ~/.oh-my-zsh/custom/functions.zsh; source ~/.zshrc"
-alias apon="curl https://raw.githubusercontent.com/Apon77/linux/junk/personal_variables.zsh > ~/.oh-my-zsh/custom/personal_variables.zsh; source ~/.zshrc"
+alias apon="curl https://raw.githubusercontent.com/Apon77/linux/junk/personal_variables.sh > ~/.oh-my-zsh/custom/personal_variables.zsh; source ~/.zshrc"
