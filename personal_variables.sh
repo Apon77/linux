7ee02379ad77bcc6692c5dedd6dc4d1a31e1450f
Apon77 <khalakuzzamanapon5@gmail.com>
2021-01-10 22:05:58 +0600

    set editor

--- personal_variables.sh
+++ personal_variables.sh
@@ -4,5 +4,6 @@ git config --global user.email "khalakuzzamanapon5@gmail.com"
 git config --global core.editor vim
 
 export TZ='Asia/Dhaka'
+export EDITOR=vim
 alias gdup='gdrive upload -p 1DuHbMh4ogKDGbjSNP-7xLPY3S1KW1Z1i'
 alias gdj='gdrive upload -p 1-0kgxGSKeco0sZsuZ0Vsk4fM1-Vs8T07'
