24c2af9934ca06bbf40e1d9b71957d1814c0ca2d
Apon77 <khalakuzzamanapon5@gmail.com>
2021-04-09 10:32:32 +0600

    whybot

--- functions.sh
+++ functions.sh
@@ -75,7 +75,7 @@ fi
 
 # Usages tg id msg
 tg(){
-bot_api=1692865707:AAEfvZh8VXOlXuHtreBzTCZqPIg6aQzvyhE
+bot_api=1692865707:AAGurCDwhaSZ0RwxIuFEYVpYo9kYJEYSwD0
 your_telegram_id=$1
 msg=$2
 curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" --data "text=$msg&chat_id=${your_telegram_id}"
