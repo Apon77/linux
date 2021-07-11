bb9b702ed12009370375419ef34d2205298b95b9
Apon77 <khalakuzzamanapon5@gmail.com>
2021-07-11 21:10:59 +0600

    Adding mouse mode tmux

--- others/append_bashrc.sh
+++ others/append_bashrc.sh
@@ -2,3 +2,5 @@ echo 'source ~/linux/aliases.sh
 source ~/linux/functions.sh
 source ~/linux/personal_variables.sh
 source ~/linux/easy.bash' >> ~/.bashrc
+
+cat ~/linux/others/tmux.conf >> ~/.tmux.conf
