bd5ff254bf71054d8546e932ec2fa56d7e7424a2
Apon77 <khalakuzzamanapon5@gmail.com>
2021-08-06 11:37:24 +0600

    adding gll

--- easy.zsh
+++ easy.zsh
@@ -8,6 +8,7 @@ alias a="curl https://raw.githubusercontent.com/Apon77/linux/junk/aliases.sh > ~
 alias easy="curl https://raw.githubusercontent.com/Apon77/linux/junk/easy.zsh > ~/.oh-my-zsh/custom/easy.zsh; source ~/.zshrc"
 alias fn="curl https://raw.githubusercontent.com/Apon77/linux/junk/functions.sh > ~/.oh-my-zsh/custom/functions.zsh; source ~/.zshrc"
 alias apon="curl https://raw.githubusercontent.com/Apon77/linux/junk/personal_variables.sh > ~/.oh-my-zsh/custom/personal_variables.zsh; source ~/.zshrc"
+alias gll=ggpull
 
 export PATH=$PATH:$HOME/.diff-so-fancy
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
