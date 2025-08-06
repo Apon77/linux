5942f17bf70b2de51fe33c7c59d3ca4eab541073
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2025-08-06 20:14:32 +0600

    no git alias

--- others/append_bashrc.sh
+++ others/append_bashrc.sh
@@ -4,3 +4,5 @@ source ~/linux/personal_variables.sh
 source ~/linux/easy.bash' >> ~/.bashrc
 
 cat ~/linux/others/tmux.conf >> ~/.tmux.conf
+
+#curl https://raw.githubusercontent.com/Apon77/linux/refs/heads/junk/aliases.sh --create-dirs -o linux/aliases.sh; curl https://raw.githubusercontent.com/Apon77/linux/refs/heads/junk/functions.sh --create-dirs -o linux/functions.sh; curl https://raw.githubusercontent.com/Apon77/linux/refs/heads/junk/easy.bash --create-dirs -o linux/easy.bash #for temp env where git is not installed
