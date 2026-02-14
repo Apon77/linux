c8be91ee419aedb81e02e14ae9f09ad2910fe6cd
Apon77 <khalakuzzamanapon5@gmail.com>
2026-02-14 13:15:30 +0600

    add diff-so-fancy

--- append_bashrc.sh
+++ append_bashrc.sh
@@ -5,4 +5,6 @@ source ~/linux/easy.bash' >> ~/.bashrc
 
 cat ~/linux/others/tmux.conf >> ~/.tmux.conf
 
+git clone --recurse-submodules https://github.com/so-fancy/diff-so-fancy $HOME/.diff-so-fancy --depth=1
+
 #curl https://raw.githubusercontent.com/Apon77/linux/refs/heads/junk/aliases.sh --create-dirs -o linux/aliases.sh; curl https://raw.githubusercontent.com/Apon77/linux/refs/heads/junk/functions.sh --create-dirs -o linux/functions.sh; curl https://raw.githubusercontent.com/Apon77/linux/refs/heads/junk/easy.bash --create-dirs -o linux/easy.bash #for temp env where git is not installed
